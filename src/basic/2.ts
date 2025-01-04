37c210c0a66f186745fafcbfed26d2a20ff02e0c
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-01-04 11:52:03 +0100

    new rep

--- src/basic/2.ts
+++
@@ -1,6 +0,0 @@
-let person: [string, number] = ["Max", 21];
-console.log(`Name: ${person[0]}`);
-console.log(`Age: ${person[1]}`);
-person = ["Anna", 25];
-console.log(`Updated Name: ${person[0]}`);
-console.log(`Updated Age: ${person[1]}`);
